7780ac006d9ee16a560ded3e157a29ee95c22335
Mortafix <moris.doratiotto@gmail.com>
2025-03-27 15:32:22 +0100

    Fix: corsaro nero torrent site

--- goldpirate/torrents/corsaronero.py
+++ goldpirate/torrents/corsaronero.py
@@ -1,22 +1,21 @@
-from functools import reduce
 from re import search, sub
 
-import requests
 from bs4 import BeautifulSoup as bs
+from requests import get
 from requests.exceptions import ConnectTimeout, ReadTimeout
 
 
 class CorsaroNero:
     def __init__(self, user_agent):
         self.url = "https://ilcorsaronero.link"
-        self.search = "/advsearch.php?search=##"
+        self.search = "/search?q=##"
         self.delimiter = "+"
-        self.sort = "/advsearch.php?search=##&&order=@@&by=DESC"
+        self.sort = "/search?q=##&sort=@@&order=desc"
         self.sort_type = {
-            "age": "data",
+            "age": "timestamp",
             "size": "size",
-            "seed": "seeds",
-            "leech": "peers",
+            "seed": "seeders",
+            "leech": "leechers",
         }
         self.page = "&page=@@"
         self.user_agent = {"User-Agent": user_agent}
@@ -31,51 +30,37 @@ class CorsaroNero:
             if sort and sort in self.sort_type
             else sub("##", sub(r"\s", self.delimiter, query), self.search)
         )
-        return reduce(
-            lambda x, y: x + y,
-            [
-                bs(
-                    requests.get(
-                        "{}{}{}".format(
-                            self.url, url_attach, sub("@@", str(i), self.page)
-                        ),
-                        headers=self.user_agent,
-                        timeout=3,
-                        verify=False,
-                    ).text,
-                    "html.parser",
-                ).findAll("td")[18:-3]
-                for i in range(pages)
-            ],
-            [],
-        )
+        titles_list = list()
+        search_texts = list()
+        for i in range(pages):
+            page_url = sub("@@", str(i), self.page)
+            soup = bs(get(f"{self.url}{url_attach}{page_url}").text, "html.parser")
+            search_texts.extend(soup.findAll("td"))
+            titles_list.extend(soup.findAll("th"))
+        return search_texts, titles_list[7:]
 
     def build_list(self, query, pages, sort=None):
         try:
-            search_list = self._search_torrents(query, pages, sort)
+            search_list, title_list = self._search_torrents(query, pages, sort)
         except (ConnectTimeout, ReadTimeout):
             return []
         search_list = [x for x in search_list if x.text and not x.get("colspan")]
         singles = [search_list[i * 6 : i * 6 + 6] for i in range(len(search_list) // 6)]
         torrents = list()
-        for torrent in singles:
+        for torrent, title in zip(singles, title_list):
             category = torrent[0].text
-            name = torrent[1].find("a").text
-            link_page = sub(
-                self.url, "", [a.get("href") for a in torrent[1].findAll("a")][0]
-            )
-            size = torrent[2].text
-            time, seed, leech = [
-                "0" if x == "n/a" else x for x in [t.text for t in torrent[3:]]
+            torrent_page = title.find("a").get("href")
+            seed, leech, size, time = [
+                "0" if x == "n/a" else x for x in [t.text for t in torrent[1:5]]
             ]
             torrents.append(
                 [
                     "CorsaroNero",
-                    name,
-                    f"{self.url}{link_page}",
-                    sub(r"(\d\d)$", r"20\1", time),
-                    category,
-                    sub(r"\d+$", "", size),
+                    title.text,
+                    f"{self.url}{torrent_page}",
+                    time.strip().split(" ")[0],
+                    category.strip(),
+                    sub(r"\d+$", "", size.strip()),
                     int(sub(",", "", seed)),
                     int(sub(",", "", leech)),
                 ]
@@ -84,7 +69,7 @@ class CorsaroNero:
 
     def get_magnet_link(self, torrent_page):
         soup = bs(
-            requests.get(torrent_page, allow_redirects=True, verify=False).text,
+            get(torrent_page, allow_redirects=True, verify=False).text,
             "html.parser",
         )
         try:
